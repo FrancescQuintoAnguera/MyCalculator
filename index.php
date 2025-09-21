d687ec3037bfaeffb0e5f9f1190a862136c15593
FrancescQuintoAnguera <francescquinto@iesmontsia.org>
2025-09-21 18:59:27 +0200

    Added history, with sessions

--- index.php
+++ index.php
@@ -1,23 +1,40 @@
 <?php
+
+session_start();
+
 // Variable
 $number1 = null;
 $number2 = null;
 $operator = null;
+$result = null;
 $arrayOperators = array('+', '-', '*', '/');
+
+// Init history
+
+if(!isset($_SESSION['history'])) {
+    $_SESSION['history'] = [];
+    $_SESSION['lastResult'] = [];
+}
 
 // function
 
 function operation($num1, $num2, $ope ){
     switch($ope){
         case '+':
-            return $num1 + $num2;
+            $result = $num1 + $num2;
+            break;
         case '-':
-            return $num1 - $num2;
+            $result = $num1 - $num2;
+            break;
         case '*':
-            return $num1 * $num2;
+            $result = $num1 * $num2;
+            break;
         case '/':
-            return $num1 / $num2;
+            $result = $num1 / $num2;
+            break;
     }
+    $_SESSION['history'][] = "$num1 $ope $num2 = $result";
+    return $result;
 };
 
 ?>
@@ -52,8 +69,33 @@ function operation($num1, $num2, $ope ){
                 $number2 = $_POST['num2'];
                 $operator = $_POST['operators'];
 
-                echo operation($number1, $number2, $operator);
+                $_SESSION['lastResult'] = operation($number1, $number2, $operator);
+                header('Location: index.php');
+                exit;
             };
+        ?>
+        <h3>Result:</h3>
+        <?php
+            if (isset($_SESSION['lastResult'])) {
+                echo '<p>'.$_SESSION["lastResult"].'</p><br>';
+                unset($_SESSION['lastResult']);
+            };
+        ?>
+        <h3>History:</h3>
+        <?php 
+            foreach ($_SESSION['history'] as $operation){
+                echo "<p>$operation</p><br>";
+            };
+        ?>
+        <form method="post">
+            <button type="submit" name="clear">Clear History</button>
+        </form>
+        <?php
+            if (isset($_POST['clear'])) {
+                $_SESSION['history'] = [];
+                header("Location: index.php");
+                exit;
+            }
         ?>
     </main>
     <footer>
